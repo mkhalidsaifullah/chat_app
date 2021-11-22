06b00bbcde5a2b9e27b75a011b4f27162f181c10
mkhalidsaifullah <khalidsaifullahmks@gmail.com>
2021-11-22 14:08:29 +0500

    connected with chat engine and css applied as App.css

--- src/App.js
+++ src/App.js
@@ -1,7 +1,19 @@
 import React from "react";
+import { ChatEngine } from 'react-chat-engine';
+import './App.css';
 
 const App = () => {
-  return <div>This is my app</div>;
-};
+  return(
+    <ChatEngine
+    
+      height = "100vh"
+      projectID="03dff7e4-6124-4ef2-9559-3709c2ab5c79"
+      userName="ad551"
+      userSecret="ChatEngine123"
+    />
+
+    
+  );
+}
 
 export default App;
